14788248ba6e749278d9670a40603c25b0b0f4b2
Luis Londono <lulondono08@gmail.com>
2023-01-29 10:08:14 -0600

    formatting

--- angie.js
+++ angie.js
@@ -5,7 +5,7 @@ const brave = "https://search.brave.com/search?q=%22Page+created+";
 const plus = "+";
 const space = "%2C+";
 const amp = "&";
-const site = "site%3Afacebook.com" 
+const site = "site%3Afacebook.com";
 const end = "&source=web";
 
 const quotes = "%22+";
@@ -13,47 +13,58 @@ const quotes = "%22+";
 // Date
 const today = new Date();
 const year = today.getFullYear();
-const mm = today.getMonth(); 
+const mm = today.getMonth();
 const dd = today.getDate();
 
 const monthNames = [
-		"January" 
-		, "February" 
-		, "March" 
-		, "April" 
-		, "May" 
-		, "June"
-  	, "July" 
-		, "August" 
-		, "September" 
-		, "October" 
-		, "November" 
-		, "December"
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 
 const month = monthNames[today.getMonth()];
 
-// timer 
+// timer
 const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
 // jobs
 const jobs = [
-		"excavation", 
-		"plumber", 
-		"electrical",
-		"roofing",
-		"landsape",
-		"pressure%2C+washer" // includes space between
+  "excavation",
+  "plumber",
+  "electrical",
+  "roofing",
+  "landsape",
+  "pressure%2C+washer", // includes space between
 ];
 
-async function load () {
-	for (let i = 0; i < jobs.length; i++) {
-		let jobName = jobs[i];
-		let final = brave.concat(month, plus, dd, space, year, quotes, site, plus, jobName, end);
+async function load() {
+  for (let i = 0; i < jobs.length; i++) {
+    let jobName = jobs[i];
+    let final = brave.concat(
+      month,
+      plus,
+      dd,
+      space,
+      year,
+      quotes,
+      site,
+      plus,
+      jobName,
+      end
+    );
 
-		createLink(final);
-		await timer(3000);
-	}
+    createLink(final);
+    await timer(3000);
+  }
 }
 
 function createLink(url) {
@@ -67,5 +78,6 @@ function createLink(url) {
 // load();
 
 // elements
-document.getElementById("load").addEventListener ("click", load);
-document.getElementById("footer").innerText = "Copyright © " + year.toString() + " | PapaDonO";
+document.getElementById("load").addEventListener("click", load);
+document.getElementById("footer").innerText =
+  "Copyright © " + year.toString() + " | PapaDonO";
